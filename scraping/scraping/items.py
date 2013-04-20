fd55730f3dc3d23a7b18a757b0f16ca760d16905
vincent <vincentnys@gmail.com>
2013-04-20 11:40:06 +0200

    Defined simple items, began work on spider.

--- scraping/scraping/items.py
+++ scraping/scraping/items.py
@@ -1,11 +1,5 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
 from scrapy.item import Item, Field
 
-class DmozItem(Item):
+class ArticleItem(Item):
     title = Field()
-    link = Field()
-    desc = Field()
+    year = Field()
--- scraping/scraping/spiders/dmoz_spider.py
+++ scraping/scraping/spiders/dmoz_spider.py
@@ -2,22 +2,12 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scraping.items import DmozItem
 
-class DmozSpider(BaseSpider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
+class JstorSpider(BaseSpider):
+    name = "jstor"
+    allowed_domains = ["jstor.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.jstor.org/action/showAdvancedSearch"
     ]
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//ul/li')
-        items = []
-        for site in sites:
-            item = DmozItem()
-            title = site.select('a/text()').extract()
-            link = site.select('a/@href').extract()
-            desc = site.select('text()').extract()
-            items.append(item)
-        return item
