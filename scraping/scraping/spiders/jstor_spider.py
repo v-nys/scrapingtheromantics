682f9a4da484665906c870c65a5a516953df4e78
vincent <vincentnys@gmail.com>
2013-04-20 12:04:39 +0200

    Working on readable implementation which uses form requests as start requests (rather than trying to figure out exact format of URL)

--- scraping/scraping/spiders/jstor_spider.py
+++ scraping/scraping/spiders/jstor_spider.py
@@ -2,12 +2,29 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scraping.items import ArticleItem
 
+# currently still a BaseSpider, not a CrawlSpider
+# need to get a better feel for Scrapy first...
 class JstorSpider(BaseSpider):
     name = "jstor"
     allowed_domains = ["jstor.org"]
-    start_urls = [
-        "http://www.jstor.org/action/showAdvancedSearch"
-    ]
 
     def parse(self, response):
+        r"""
+        Return items scraped (as `ArticleItem`s) and further
+        pages to be scraped (as `Request`s).
+        """
+        filename = 'AdvancedSearch'
         hxs = HtmlXPathSelector(response)
+        with open(filename, 'wb') as fh:
+            fh.write(response.body)
+
+    def start_requests(self)
+        r"""
+        Return an iterable containing the initial requests for
+        the JStor spider.
+
+        These requests are `FormRequest`s which use JStor's
+        advanced search functionality.
+        """
+        return (FormRequest('http://www.jstor.org/action/showAdvancedSearch')
+                for poet in ['Byron', 'Keats', 'Shelley'])
